f86656cbf65bc7c6fbf02506a8aaea82c8356067
Lee Azzarello <lee@rockingtiger.com>
2019-03-09 08:15:43 +0700

    Expose params for chaos

--- scripts/lazzarello/chaos.lua
+++ scripts/lazzarello/chaos.lua
@@ -1,5 +1,20 @@
 engine.name = "Chaos"
 
+local hid = require 'hid'
+local ControlSpec = require "controlspec"
+
 function init()
   print("I'm loud")
+  params:add{type="control", id="hz", name= "Sine Freqency",
+    controlspec=ControlSpec.new(10,10000,"exp",10,3000,"hz"),
+    action = engine.hz
+  }
+  params:add{type="control", id="amp", name= "Sine Amplitude",
+    controlspec=ControlSpec.new(0.1,100.0,"lin",0,10,"db"),
+    action = engine.amp
+  }
 end
+
+local tablet = nil
+--tablet = hid.findDeviceSuporting('ABS_PRESSURE')
+--tablet.print()
